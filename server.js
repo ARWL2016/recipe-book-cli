6c2f75ff5fba0c409b2901f8e0ac63f6019f6062
Alistair Willis <alistairrwillis@gmail.com>
2017-12-24 21:44:03 +0900

    add caching

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const compression = require('compression');
 const helmet = require('helmet');
 const path = require('path');
+const ms = require('ms');
 
 const app = express();
+const staticOptions = {maxAge: '10d'};
+
 app.use(helmet());
 app.use(compression());
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist'), staticOptions));
 
 app.get('*', (req, res) => {
+  res.setHeader('Cache-Control', 'no-store');
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 
