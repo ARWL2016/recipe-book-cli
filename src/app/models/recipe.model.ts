608bbe90575c8dc0528e1e518ef3466100384881
Alistair Willis <alistairrwillis@gmail.com>
2020-05-11 14:58:16 +0100

    prod build

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,12 +1,14 @@
+export interface Ingredient {
+  ingredient: string;
+  amount?: string;
+}
+
 export class Recipe {
   constructor(
     public recipeName: string = '',
-    public serves: string = '1',
-    public ingredients: [{
-      ingredient: string,
-      amount?: string
-    }] = [{ingredient: ''}],
+    public serves: string = '',
+    public ingredients: Ingredient[] = [{ ingredient: '' }],
     public method: string = '',
     public id?: string
-  ) {}
+  ) { }
 }
